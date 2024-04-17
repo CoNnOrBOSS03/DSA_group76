ed9e36cea5e89f1b8d6522f662956c1f71f6935d
CoNnOrBOSS03 <connorverra@gmail.com>
2024-04-17 13:46:07 -0400

    made a bunch of progress on main, running should give you a link to view the map of the graph we're using

---
+++ location_coords.py
@@ -0,0 +1,3 @@
+# using this file to store coordinates of locations for users to pick!
+
+TURLINGTON_HALL = [29.64928, 82.34400]
--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 from bridges.data_src_dependent import data_source
 from bridges.bridges import Bridges
+from math import sqrt
+from location_coords import *
 import os
 import queue as Q
 
 
-# return the vertex the closest to some lat/lon coordinate
-def getClosest(gr, lat, lon):
-    # TODO
+# helper function: returns distance between vertex and given coordinates using pythagorean theorem
+def getDistance(vertex, lat, lon):
+    return sqrt((vertex.latitude - lat) ** 2 + (vertex.longtitude - lon) ** 2)
 
-    return -1
+
+# return the vertex the closest to some lat/lon coordinate
+# can use this to figure out a defined vertex based on the location the user wants to start from/end at
+def getClosest(gr, lat, lon):
+    #
+    # thinking we iterate through all vertices, find the one where distance from desired coordinates and
+    # vertex coordinates is smallest, return it
+
+    vertices = gr.vertices
+    current_closest_vertex = vertices[0]
+    current_shortest_distance = 100  # arbitrary big number
+
+    # find distance from using pythagorean theorem ig right?
+    for vertex in vertices:
+        distance = getDistance(current_closest_vertex, lat, lon)
+        if distance < current_shortest_distance:
+            current_closest_vertex = vertex
+            current_shortest_distance = distance
+
+    return current_closest_vertex
 
 
 # styling the source vertex
@@ -53,16 +74,14 @@ def style_parent(gr, parent, dest):
 
 def main():
     # get data
-    bridges = Bridges(209, "BRIDGES_USER_ID", "BRIDGES_API_KEY")
+    bridges = Bridges(209, "CoNnOrBOSS03", "996702144742")
 
-    bridges.set_title("Graph : OpenStreet Map Example")
+    bridges.set_title("Graph : Gainesville Map Graph Test")
     bridges.set_description(
-        "OpenStreet Map data of Charlotte downtown area, with colors based on distance from the center of downtown")
+        "Shows residential paths in Gainesville!")
 
     # TODO: Get Data
-    # osm_data = data_source.get_osm_data("Charlotte, North Carolina", "primary")
-    # osm_data = data_source.get_osm_data("Charlotte, North Carolina", "secondary")
-    osm_data = data_source.get_osm_data(35.28, -80.8, 35.34, -80.7, "tertiary");  # UNCC Campus
+    osm_data = data_source.get_osm_data("Gainesville, Florida", "residential")
 
     gr = osm_data.get_graph()
     gr.force_large_visualization(True)
@@ -71,10 +90,13 @@ def main():
     # #find and style the root of the Shortest Path
     # root = getClosest(gr,
     #                      (osm_data.latitude_range[0]+osm_data.latitude_range[1])/2,
-    #                      (osm_data.longitude_range[0]+osm_data.longitude_range[1])/2)
+    #                      (osm_data.longitude_range[0]+osm_data.longitude_range[1])/2)  # sets origin to center of graph
+    # FIXME: getClosest doesn't work
+    # root = getClosest(gr, TURLINGTON_HALL[0], TURLINGTON_HALL[1])  # sets origin to turlington
+
     # style_root(gr, root)
-    # bridges.set_data_structure(gr)
-    # bridges.visualize()
+    bridges.set_data_structure(gr)
+    bridges.visualize()
 
     # TODO: Uncomment for part 3
     # #run Shortest Path
