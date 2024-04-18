c35453afa89398405735aa1b8e06e2189e6b08de
CoNnOrBOSS03 <connorverra@gmail.com>
2024-04-18 12:41:03 -0400

    added more locations to location dictionary

--- location_coords.py
+++ location_coords.py
@@ -3,6 +3,11 @@
 LocationDictionary = {}
 LocationDictionary["Turlington Hall"] = [29.64928, 82.34400]
 LocationDictionary["Marston Library"] = [29.64822, 82.34435]
+LocationDictionary["Beaty Towers"] = [29.64402, 82.34084]
+LocationDictionary["Carleton Auditorium"] = [29.64907, 82.34161]
+LocationDictionary["Reitz Union"] = [29.64630, 82.34769]
+LocationDictionary["Malachowsky Hall"] = [29.64410, 82.34907]
+LocationDictionary["UF Shands"] = [29.64003, 82.34464]
 
 LOCATION_NAMES = []
 
