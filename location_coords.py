c8b7d36fe90011105cc3b66a52213b129ef7e2c7
CoNnOrBOSS03 <connorverra@gmail.com>
2024-04-18 12:31:58 -0400

    reformatted into a dictionary, creates a list of names to pass to the main screen for use in comboboxes

--- location_coords.py
+++ location_coords.py
@@ -1,3 +1,12 @@
 # using this file to store coordinates of locations for users to pick!
 
-TURLINGTON_HALL = [29.64928, 82.34400]
+LocationDictionary = {}
+LocationDictionary["Turlington Hall"] = [29.64928, 82.34400]
+LocationDictionary["Marston Library"] = [29.64822, 82.34435]
+
+LOCATION_NAMES = []
+
+for key in LocationDictionary:
+    LOCATION_NAMES.append(key)
+
+
