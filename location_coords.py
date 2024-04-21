25c37598a90d4ba1bd06b6d5bee8f6aaa78b9218
CoNnOrBOSS03 <connorverra@gmail.com>
2024-04-21 17:43:11 -0400

    removed malachosky, it was causing issues due to a disconnected portion of the OSMdata graph

--- location_coords.py
+++ location_coords.py
@@ -6,7 +6,6 @@ LocationDictionary["Marston Library"] = [29.64822, -82.34435]
 LocationDictionary["Beaty Towers"] = [29.64402, -82.34084]
 LocationDictionary["Carleton Auditorium"] = [29.64907, -82.34161]
 LocationDictionary["Reitz Union"] = [29.64630, -82.34769]
-LocationDictionary["Malachowsky Hall"] = [29.64410, -82.34907]
 LocationDictionary["UF Shands"] = [29.64003, -82.34464]
 
 LOCATION_NAMES = []
