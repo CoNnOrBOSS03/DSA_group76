53d6334390282f2c6c5a19306eea2fabb826fb9b
CoNnOrBOSS03 <connorverra@gmail.com>
2024-04-22 15:01:29 -0400

    progress on dijkstra's, ironing out issues

--- main.py
+++ main.py
@@ -41,19 +41,58 @@ def shortestPathArray(gr, root):
 
     # need to initialize the arrays and sets
     # set S: initially empty
+
+    # FIXME: doesnt work because the same distance, being of the root, is always found
+    # need to save distances somehow with the vertices to process
+
     computed_vertices = set()
     # set V-S: need to place all vertices into it
     vertices_to_process = set()
     for vertex in vertices:
         vertices_to_process.add(vertex)
     # d[v]: need to set all to infinity besides source, which will be set to 0
-    distances = [float('inf')] * len(vertices)
+    distances = [1e6] * len(vertices)
     distances[root] = 0
     # p[v]: need to set all to -1
     predecessors = [-1] * len(vertices)
 
     # time for the actual algorithm
 
+    while min(distances) != 1e6:
+
+        # first, start w/ vertex w/ minimum distance in d[v], THAT HASN'T BEEN COMPUTED, and add to set S
+        # index should directly correlate with vertex id so should work?
+
+        minVertexID = distances.index(min(distances))
+
+        # test to see if this is correct
+        # print(minVertexID)
+        # print(root)
+        # print(f"[{gr.get_vertex_data(root).latitude}, {gr.get_vertex_data(root).longitude}]")
+        # print(f"[{gr.get_vertex_data(minVertexID).latitude}, {gr.get_vertex_data(minVertexID).longitude}]")
+
+        # need to move vertex from not computed set to computed set
+        # FIXME: removing vertex fucks up the indexing?
+        vertices_to_process.remove(minVertexID)
+        computed_vertices.add(minVertexID)
+
+        #print(computed_vertices)
+
+        # now, need to process edges adjacent to the first vertex, and update distances based on relaxation!
+
+        # testing getting adjacent edges
+        # for edge in gr.out_going_edge_set_of(minVertexID):
+        #     print(f"{edge.fromv} ---> {edge.tov}, weight = {edge._edge_data}")
+        #     gr.get_vertex(edge.tov).color = "orange"
+
+        # gets outgoing edges
+        for edge in gr.out_going_edge_set_of(minVertexID):
+            # relaxation!
+            if distances[edge.tov] > distances[edge.fromv] + edge._edge_data:
+                distances[edge.tov] = distances[edge.fromv] + edge._edge_data
+                predecessors[edge.tov] = edge.fromv
+                print(f"Predecessor: {predecessors[edge.tov]}")
+                print(f"Distance: {distances[edge.tov]}")
 
 
     # TODO
@@ -86,14 +125,12 @@ def style_parent(gr, parent, dest):
 
 
 def main():
+
     # get data
     bridges = Bridges(209, "CoNnOrBOSS03", "996702144742")
-
     bridges.set_title("Graph : Gainesville Map Graph Test")
-    bridges.set_description(
-        "Shows residential paths in Gainesville!")
+    bridges.set_description("Shows residential paths in Gainesville!")
 
-    # TODO: Get Data
     osm_data = data_source.get_osm_data("Gainesville, Florida", "residential")
     # alternatively, use bounding box for more specified region
     #osm_data = data_source.get_osm_data(gainesville_bounding_box[0], gainesville_bounding_box[1],
@@ -102,31 +139,28 @@ def main():
     gr = osm_data.get_graph()
     gr.force_large_visualization(True)
 
-    # TODO: Uncomment for part 2
     # #find and style the root of the Shortest Path
-    # root = getClosest(gr,
-    #                      (osm_data.latitude_range[0]+osm_data.latitude_range[1])/2,
-    #                      (osm_data.longitude_range[0]+osm_data.longitude_range[1])/2)  # sets origin to center of graph
     root = getClosest(gr, LocationDictionary["Turlington Hall"][0], LocationDictionary["Turlington Hall"][1])  # sets origin to turlington
-    vertex = gr.get_vertex_data(root)
-    #print(f"[{vertex.latitude}, {vertex.longitude}]")
+    # print(root)
+    # vertex = gr.get_vertex_data(root)
+    # print(f"[{vertex.latitude}, {vertex.longitude}]")
 
     for key in gr.vertices:
         gr.get_vertex(key).opacity = 0.5
 
     style_root(gr, root)
-    bridges.set_data_structure(gr)
-    bridges.visualize()
+
 
     # TODO: Uncomment for part 3
     # #run Shortest Path
     # Shouldn't we give an option between Dijkstra's algorithm and another shortest path algorithm so that we can compare the two? - Andres
-    # (distance,parent) = shortestPath(gr,root)
+    # (distance,parent) =
+    shortestPathArray(gr, root)
 
     # #style vertices based on distances
     # style_distance(gr, distance)
-    # bridges.set_data_structure(gr)
-    # bridges.visualize()
+    bridges.set_data_structure(gr)
+    bridges.visualize()
 
     # TODO Uncomment for part 4
     # #find a destination
