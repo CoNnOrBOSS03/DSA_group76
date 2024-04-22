4712c486818b1b45ba2dafeb540e5fc88ee9885f
CoNnOrBOSS03 <connorverra@gmail.com>
2024-04-22 18:07:02 -0400

    deleted unneeded code

--- main_functions.py
+++ main_functions.py
@@ -5,10 +5,6 @@ from location_coords import *
 from heapq import *
 import time
 
-
-gainesville_bounding_box = [29.69267, -82.38941, 29.65922, -82.32507]
-
-
 # helper function: returns distance between vertex and given coordinates using pythagorean theorem
 def getDistance(vertex, lat, lon):
     return sqrt((vertex.latitude - lat) ** 2 + (vertex.longitude - lon) ** 2)
@@ -120,7 +116,6 @@ def ShortestPathHeap(gr, root, destination):
         # first, start w/ vertex w/ minimum distance in d[v], THAT HASN'T BEEN COMPUTED, and add to set S
         # index should directly correlate with vertex id so should work?
 
-        # FIXME: make work with heap
         minVertexID = heappop(not_computed_distances_heap)[1][0]
         #print(minVertexID)
         # make the distance at that index arbitrary large so we don't reselect it
@@ -140,96 +135,8 @@ def ShortestPathHeap(gr, root, destination):
                 distances[edge.tov] = distances[edge.fromv] + edge._edge_data
                 not_computed_distances[edge.tov] = distances[edge.fromv] + edge._edge_data
                 predecessors[edge.tov] = edge.fromv
-                # print(f"Predecessor: {predecessors[edge.tov]}")
-            # print(f"Distance: {distances[edge.tov]}")
-
-    # print(f"Distance of chosen path is {distances[destination]}")
-
-
-    # color path
-    dest = destination
-    source = predecessors[destination]
-
-    # while dest != root:
-    #     gr.get_link_visualizer(source, dest).color = "orange"
-    #     gr.get_link_visualizer(source, dest).thickness = 5
-    #     dest = source
-    #     source = predecessors[dest]
-
-    #print(round((round(time.time() - start_time, 10)) * 10e3, 3))
 
     return round((round(time.time() - start_time, 10)) * 10e3, 3)
 
-
 # style the graph based on distance
-def style_distance(gr, distance):
-    # TODO
-    # find the maximum distance which is not infinity
 
-    # style the color of vertices with a linear scale
-    pass
-
-
-# style the path between the root and the destination (root is not given because all parent path goes to root)
-def style_parent(gr, parent, dest):
-    # TODO
-
-    # style all vertices to almost invisible
-
-    # style all edges to almost invisible
-
-    # Style the path between parent and dest in black
-    ##style node
-
-    ##style edge along the path
-    pass
-
-
-def main():
-
-    # get data
-    bridges = Bridges(209, "CoNnOrBOSS03", "996702144742")
-    bridges.set_title("Graph : Gainesville Map Graph Test")
-    bridges.set_description("Shows residential paths in Gainesville!")
-
-    osm_data = data_source.get_osm_data("Gainesville, Florida", "residential")
-    # alternatively, use bounding box for more specified region
-    #osm_data = data_source.get_osm_data(gainesville_bounding_box[0], gainesville_bounding_box[1],
-                                        #gainesville_bounding_box[2], gainesville_bounding_box[3], "residential")
-
-    gr = osm_data.get_graph()
-    gr.force_large_visualization(True)
-
-    # #find and style the root of the Shortest Path
-    root = getClosest(gr, LocationDictionary["Turlington Hall"][0], LocationDictionary["Turlington Hall"][1])  # sets origin to turlington
-    destination = getClosest(gr, LocationDictionary["UF Shands"][0], LocationDictionary["UF Shands"][1])
-    # print(root)
-    # vertex = gr.get_vertex_data(root)
-    # print(f"[{vertex.latitude}, {vertex.longitude}]")
-
-    for key in gr.vertices:
-        gr.get_vertex(key).opacity = 0.5
-
-    style_root(gr, root)
-
-
-    time = ShortestPathHeap(gr, root, destination)
-
-    print(f"time = {time}")
-
-    # #style vertices based on distances
-    # style_distance(gr, distance)
-    bridges.set_data_structure(gr)
-    bridges.visualize()
-
-    # TODO Uncomment for part 4
-    # #find a destination
-    # dest = getClosest(gr,
-    #                   (osm_data.latitude_range[0]+(osm_data.latitude_range[1]-osm_data.latitude_range[0])/4),
-    #                   (osm_data.longitude_range[0]+(osm_data.longitude_range[1]-osm_data.longitude_range[0])/4))
-
-    # #style the path from root to destination
-    # style_parent(gr,parent, dest)
-
-    # bridges.set_data_structure(gr)
-    # bridges.visualize()
